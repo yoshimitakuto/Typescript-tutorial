ca2507804d3fee76eed596644bd46d14a64862eb
Yoshimi Takuto <yocchantaku0127@gmail.com>
2023-08-30 00:05:29 +0900

    チュートリアル完了

--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,8 @@ module.exports = {
         path: `${__dirname}/dist`,
         filename: "bundle.js",
     },
-    // mode: "development",
-    mode: "production",
+    // mode: "development",　//開発モード
+    mode: "production", //本番モード
     resolve: {
         extensions: [".ts", ".js"],
     },
