0de58c6ccc84b79ddf9ae470d894c09b74d3fd2b
Yoshimi Takuto <yocchantaku0127@gmail.com>
2023-08-30 00:02:59 +0900

    hello typescript 表示完了

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,22 @@
-// どこに何をバンドルしていくのかを決めるためのファイル
-
 module.exports = {
-    // エントリーポイント（どこの何をコンパイルするのかを決める）
     entry: {
-        // バンドルするファイルを指定する
-        bundle: "./src/index.js",
+        bundle: "./src/index.ts",
     },
-    // エンドポイント（何をどこに出力するのか）
     output: {
-        // __dirname(root_pathのことを指している)
         path: `${__dirname}/dist`,
         filename: "bundle.js",
     },
-    // 開発者モード設定
     mode: "development",
-    resolved: {
-        // 自動的に拡張子を設定してもらうための記述
+    resolve: {
         extensions: [".ts", ".js"],
     },
     devServer: {
-        // サーバー起動時は以下のディレクトリを参照し、自動で開いてくださいという記述
         static: {
             directory: `${__dirname}/dist`,
         },
         open: true,
     },
-    modeul: {
-        // ルールを指定
+    module: {
         rules: [
             {
                 test: /\.ts$/,
