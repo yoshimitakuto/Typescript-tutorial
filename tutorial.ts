29fc1da3ae6a685669c929e09bae6260fc6f936a
Yoshimi Takuto <yocchantaku0127@gmail.com>
2023-08-29 22:56:20 +0900

    型定義（現段階ではWebpackを使用した環境構築をしていない状態）

---
+++ tutorial.ts
@@ -0,0 +1,9 @@
+// const a: Number = 3;
+// const b: Number = a;
+// console.log(b);
+
+// 配列（数値）指定方法 (let array1: Array<number> = [1,2,3,4,5])
+let array1: number[] = [1,2,3,4,5];
+let array2: number[] = [];
+array2 = array1;
+console.log(array2);
